3d659535dbe9464039ea4ebb927f680ab33b1aaa
NOOB2033 <zaza322228@mail.ru>
2022-06-30 06:05:31 +0700

    Добавил считывание данных с Json

--- dataReader.cpp
+++ dataReader.cpp
@@ -13,11 +13,36 @@ void SqlLiteReader::readData(const QString& path, DataList& data) {
         }
     } else {
         QMessageBox messageBox;
-        messageBox.setText("нель");
+        messageBox.setText("нельзя открыть файл");
         messageBox.exec();
     }
 }
 
 
 void JsonReader::readData(const QString& path, DataList& data) {
+    QFile file(path);
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        QMessageBox messageBox;
+        messageBox.setText("Нельзя открыть файл");
+        messageBox.exec();
+    }
+
+    QString string = file.readAll();
+    QJsonDocument json = QJsonDocument::fromJson(string.toUtf8());
+
+    if (!json.isArray()) {
+        QMessageBox messageBox;
+        messageBox.setText("Введите Json как Массив");
+        messageBox.exec();
+    }
+    data.clear();
+    size_t count = 0;
+    for (auto value : json.array()) {
+        if (value.isObject() && count < 10) {
+            QPointF point(count, value.toObject()["Value"].toDouble());
+            data.push_back(Data(point, value.toObject()["Time"].toString()));
+            ++count;
+        }
+    }
 }
