853e972ecf61b0e246d6bd8d55e1812e5ab69e2b
NOOB2033 <zaza322228@mail.ru>
2022-07-01 13:53:14 +0700

    Добавил комментарии в mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,209 +1,159 @@
 #include "mainwindow.h"
 
-
+// Конструктор главного окна
 MainWindow::MainWindow(QMainWindow* parent) : QMainWindow(parent) {
-    setGeometry(100, 100, 1600, 800);
-    setWindowTitle("Диаграмма");
+    setGeometry(100, 100, 1500, 600); // Задаем размер
+    setWindowTitle("Диаграмма");                    // Название
 
-    chart = new Chart;
+    chart = new Chart;                              // Создаем диаграмму
 
-    fileModel = new QFileSystemModel(this);
+    fileModel = new QFileSystemModel(this);   // Создаем модель файловой системы
     fileModel->setFilter(QDir::NoDotAndDotDot | QDir::Files);
-    fileModel->setRootPath(homePath);
-
+    fileModel->setRootPath(homePath); // Задаем путь
+    // Создаем виджеты
     tableW = new tableWidget(fileModel);
     chartW = new chartWidget(&chart->getView());
-
+    // Создаем сплиттер и добавляем ему виджеты
     QSplitter* splitter = new QSplitter;
     splitter->addWidget(tableW);
     splitter->addWidget(chartW);
-
+    // Делаем сплитер центральным виджетом
     setCentralWidget(splitter);
-
+    // Задаем модель для отслеживания файлов
     selectionModel = tableW->tableView->selectionModel();
-
+    // Выполняем соединения сингалов-слотов
     connections();
 }
 
-
+// Конструктор виджета, отвечающего за таблицу папок
 MainWindow::tableWidget::tableWidget(QFileSystemModel* fileModel, QWidget* parent) : QWidget(parent) {
+    // Создаем табличное представление
     tableView = new QTableView;
     tableView->setModel(fileModel);
-
+    // Создаем кнопку
     openButton = new QPushButton ("Открыть папку");
+    // Создаем вертикальный Layout
     QVBoxLayout* table = new QVBoxLayout;
     table->addWidget(new QLabel("Выберите базу данных:"));
     table->addWidget(tableView);
     table->addWidget(openButton);
-
+    // добавляем layout на виджет
     setLayout(table);
 }
 
-
+// Конструктор виджета, отвечающего за диаграмму
 MainWindow::chartWidget::chartWidget(QChartView* view, QWidget* parent) : QWidget(parent) {
+    // Создаем кнопку
     printButton = new QPushButton("Печать графика");
+    // Создаем чекбокс
     checkboxColor = new QCheckBox("Черно-белый");
-
+    // Создаем combobox
     boxType = new QComboBox(); // Выбор типа графика
     boxType->insertItem(0, QString("BarChart"));
     boxType->insertItem(1, QString("PieChart"));
-
+    // Создаем горизонтальный layout
     QHBoxLayout* horizontal = new QHBoxLayout;
     horizontal->addWidget(new QLabel("Выберите тип диаграммы:"));
     horizontal->addWidget(boxType);
     horizontal->addWidget(checkboxColor);
     horizontal->addWidget(printButton);
-
+    // Создаем вертикальный layout
     QVBoxLayout* vertical = new QVBoxLayout;
     vertical->addLayout(horizontal);
     vertical->addWidget(view);
-
+    // добавляем layout на виджет
     setLayout(vertical);
 }
 
-
+// Функция соединения сигнало-слотов
 void MainWindow::connections() {
     connect(tableW->openButton, SIGNAL(clicked()),this,SLOT(directoryChoose()));
     connect(chartW->boxType,SIGNAL(currentTextChanged(const QString&)),this,SLOT(comboboxChange()));
     connect(chartW->checkboxColor, SIGNAL(toggled(bool)), this, SLOT(colorChange()));
     connect(chartW->printButton,SIGNAL(clicked()), this, SLOT(toPDF()));
     connect(selectionModel,SIGNAL(selectionChanged(const QItemSelection &, const QItemSelection &)),this,SLOT(selectionChange(const QItemSelection &, const QItemSelection &)));
-
+    // Добавляем в контейнер по умолчанию вертикальный типа диаграммы
     IOCContainer::instance().RegisterInstance<IchartDrawer, barChartDrawer>();
 }
 
-
+// Слот обработки выбора выбранной папки
 void MainWindow::directoryChoose() {
-    QFileDialog dialog(this);
+    QFileDialog dialog(this); // Открываем диалог
     dialog.setFileMode(QFileDialog::Directory);
     if (dialog.exec()) {
-        homePath = dialog.selectedFiles().first();
+        homePath = dialog.selectedFiles().first(); // Получаем выбранную директорию
     }
-    tableW->tableView->setRootIndex(fileModel->setRootPath(homePath));
+    tableW->tableView->setRootIndex(fileModel->setRootPath(homePath)); // устанавливаем этот путь
 }
 
-
+// Слот обработки выбора типа диаграммы
 void MainWindow::comboboxChange() {
 
-    if (chartW->boxType->currentText() == "BarChart") {
+    if (chartW->boxType->currentText() == "BarChart") { // Задаем вертикальную диаграмму
         IOCContainer::instance().RegisterInstance<IchartDrawer, barChartDrawer>();
         chart->updateChart();
-    } else {
+    } else { // в противном случае - круговую
         IOCContainer::instance().RegisterInstance<IchartDrawer, pieChartDrawer>();
         chart->updateChart();
     }
 }
 
-
+// Слот смены цвета
 void MainWindow::colorChange() {
-    chart->changeColor();
-    chart->updateChart();
+    chart->changeColor(); // Меняем цвет
+    chart->updateChart(); // Обновляем диаграмму
 }
 
-
+// Слот печать диаграммы в PDF
 void MainWindow::toPDF() {
-    if (checkPrint) {
-        QFileDialog dialog(this);
+    if (checkPrint) { // Если можно печатать
+        QFileDialog dialog(this); // создаем диалог
         dialog.setViewMode(QFileDialog::Detail);
         QString path;
-        if (dialog.exec()) {
+        if (dialog.exec()) { // Получаем из диалога путь сохранения
             path = dialog.selectedFiles().first();
-        }
+        } // Добавляем расширение
         QPdfWriter writer(path + ".pdf");
-        writer.setCreator("Creator");
-        writer.setPageSize(QPagedPaintDevice::A4);
+        writer.setCreator("Creator"); // указываем создателя
+        writer.setPageSize(QPagedPaintDevice::A4); // размер страницы
         QPainter painter(&writer);
-        chart->getView().render(&painter);
+        chart->getView().render(&painter); // Печатаем
         painter.end();
-    } else {
+    } else { // В противном случае сообщение об ошибке
         QMessageBox messageBox;
-        messageBox.setText("Отсутствует диаграмма чтобы напечатать");
+        messageBox.setText("Ошибка! Отсутствует диаграмма чтобы напечатать");
         messageBox.exec();
     }
 }
 
-
+// Слот для выбора файла
 void MainWindow::selectionChange(const QItemSelection& selected, const QItemSelection& deselected) {
     Q_UNUSED(deselected);
-
+    // Получаем путь к файалу
     QString path(fileModel->filePath(selected.indexes().constFirst()));
 
-
+    // если формат .sqllite
     if (path.endsWith(".sqlite")) {
-        checkPrint = true;
+        checkPrint = true; // Разрешаем печатать
+        // Добавляем в контейнер
         IOCContainer::instance().RegisterInstance<IdataReader, SqlLiteReader>();
+        // Обновляем диаграмму
         chart->updateData(path);
         chart->updateChart();
         return;
-    }
+    } // если формат .Json
     if (path.endsWith(".json")) {
-        checkPrint = true;
+        checkPrint = true; // Разрешаем печатать
+        // Добавляем в контейнер
         IOCContainer::instance().RegisterInstance<IdataReader, JsonReader>();
+        // Обновляем диаграмму
         chart->updateData(path);
         chart->updateChart();
         return;
     }
-
+    // В противном случае запрещаем распечатывать и выдаем сообщение об ошибке
     checkPrint = false;
     QMessageBox messageBox;
-    messageBox.setText("Неверный формат");
+    messageBox.setText("Ошибка! Неверный формат");
     messageBox.exec();
 }
-
-
-//tableWidget::tableWidget(QWidget *parent) {
-//    fileModel = new QFileSystemModel(this);
-//    fileModel->setFilter(QDir::NoDotAndDotDot | QDir::Files);
-//    fileModel->setRootPath(homePath);
-//
-//    tableView = new QTableView;
-//    tableView->setModel(fileModel);
-//
-//    openButton = new QPushButton ("Открыть папку");
-//
-//    selectionModel = tableView->selectionModel();
-//
-//    connect(openButton, SIGNAL(clicked()),this,SLOT(directoryChoose()));
-//    connect(selectionModel,SIGNAL(selectionChanged(const QItemSelection &, const QItemSelection &)),this,SLOT(selectionChange(const QItemSelection &, const QItemSelection &)));
-//
-//    QVBoxLayout* tableLayout = new QVBoxLayout;
-//    tableLayout->addWidget(new QLabel("Выберите базу данных:"));
-//    tableLayout->addWidget(tableView);
-//    tableLayout->addWidget(openButton);
-//}
-//
-//
-//void tableWidget::directoryChoose() {
-//    QFileDialog dialog(this);
-//    dialog.setFileMode(QFileDialog::Directory);
-//    if (dialog.exec()) {
-//        homePath = dialog.selectedFiles().first();
-//    }
-//    tableView->setRootIndex(fileModel->setRootPath(homePath));
-//}
-//
-//void tableWidget::selectionChange(const QItemSelection& selected, const QItemSelection& deselected) {
-//    Q_UNUSED(deselected);
-//
-//    QString path(fileModel->filePath(selected.indexes().constFirst()));
-//
-////    if (path.endsWith(".sqlite")) {
-////        checkPrint = true;
-////        IOCContainer::instance().RegisterInstance<IdataReader, SqlLiteReader>();
-////        chart->updateData(path);
-////        chart->updateChart();
-////        return;
-////    }
-////    if (path.endsWith(".json")) {
-////        checkPrint = true;
-////        IOCContainer::instance().RegisterInstance<IdataReader, JsonReader>();
-////        chart->updateData(path);
-////        chart->updateChart();
-////        return;
-////    }
-//
-////    checkPrint = false;
-////    QMessageBox messageBox;
-////    messageBox.setText("Неверный формат");
-////    messageBox.exec();
-//}
--- mainwindow.h
+++ mainwindow.h
@@ -4,70 +4,54 @@
 
 #include "Chart.h"
 
-
+// Класс главного окна
 class MainWindow : public QMainWindow {
 Q_OBJECT
 public:
+    // Конструкторы и деструктор
     MainWindow(QMainWindow* parent = nullptr);
     ~MainWindow() = default;
-
+// Слоты
 private slots:
-    void directoryChoose();
-    void comboboxChange();
-    void colorChange();
-    void toPDF();
+    void directoryChoose(); // Обработка выбора папки
+    void comboboxChange();  // Обработка выбора типа диаграммы
+    void colorChange();     // Обработка выбора цвета диаграммы
+    void toPDF();           // Обработка сохранения диаграммы в PDF
+    // Обработка выбора выбранного файла
     void selectionChange(const QItemSelection& selected, const QItemSelection& deselected);
 
 private:
-
+    // Приватный класс-виджет, отвечающий за табличное представление папок
     class tableWidget : public QWidget {
     public:
+        // Конструктор, принимающий файловую модель
         tableWidget(QFileSystemModel* fileModel, QWidget* parent = nullptr);
 
-        QTableView* tableView = nullptr;
-        QPushButton* openButton = nullptr;
+        QTableView* tableView = nullptr;  // Табличной представление
+        QPushButton* openButton = nullptr;// Кнопка "Открыть папку"
     };
-
+    // Приватный класс-виджет, отвечающий за диаграмму
     class chartWidget : public QWidget {
     public:
+        // Конструктор, принимающий представление диаграммы
         chartWidget(QChartView* view, QWidget* parent = nullptr);
 
-        QPushButton* printButton = nullptr;
-        QCheckBox* checkboxColor = nullptr;
-        QComboBox* boxType = nullptr;
+        QPushButton* printButton = nullptr; // Кнопка "Печать графика"
+        QCheckBox* checkboxColor = nullptr; // Чекбокс для смены цвета
+        QComboBox* boxType = nullptr;       // Список видов диаграмм
     };
+    // Поля
+    bool checkPrint = false; // Проверка на то, можно ли печатать PDF
 
-    bool checkPrint = false;
+    QString homePath = QDir::homePath();           // Текущий путь
+    Chart* chart = nullptr;                        // Диаграмма
+    QFileSystemModel* fileModel = nullptr;         // Файловая модель
+    tableWidget* tableW = nullptr;                 // Объект класса виджета, отвечающего за табличное представление
+    chartWidget* chartW = nullptr;                 // Объект класса виджета, отвечающего за диаграмму
+    QItemSelectionModel* selectionModel = nullptr; // Модель для отслеживания элементов
 
-    QString homePath = QDir::homePath();
-    Chart* chart = nullptr;
-    QFileSystemModel* fileModel = nullptr;
-    tableWidget* tableW = nullptr;
-    chartWidget* chartW = nullptr;
-    QItemSelectionModel* selectionModel = nullptr;
-
-    void connections();
+    void connections();                            // Функция, соединяющая сигналы-слоты
 };
-
-
-//class tableWidget : QWidget {
-//Q_OBJECT
-//public:
-//    tableWidget(QWidget* parent = nullptr);
-//
-//    QString getPath();
-//private slots:
-//    void directoryChoose();
-//    void selectionChange(const QItemSelection& selected, const QItemSelection& deselected);
-//
-//    private:
-//    QString homePath = QDir::homePath();
-//
-//    QFileSystemModel* fileModel = nullptr;
-//    QTableView* tableView = nullptr;
-//    QPushButton* openButton = nullptr;
-//    QItemSelectionModel* selectionModel = nullptr;
-//};
 
 
 #endif // MAINWINDOW_H
